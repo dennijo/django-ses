71b7dcc077d7a6a875f5a006506a190b605ecb05
Josh Dennis <josh@joshdennis.net>
2018-04-23 16:27:55 -0500

    New signals for reject, open, click

--- django_ses/signals.py
+++ django_ses/signals.py
@@ -6,3 +6,9 @@ bounce_received = Signal(providing_args=["mail_obj", "bounce_obj", "raw_message"
 complaint_received = Signal(providing_args=["mail_obj", "complaint_obj", "raw_message"])
 
 delivery_received = Signal(providing_args=["mail_obj", "delivery_obj", "raw_message"])
+
+reject_received = Signal(providing_args=["mail_obj", "reject_obj", "raw_message"])
+
+open_received = Signal(providing_args=["mail_obj", "open_obj", "raw_message"])
+
+click_received = Signal(providing_args=["mail_obj", "click_obj", "raw_message"])
